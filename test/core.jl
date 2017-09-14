pegus2344/julia-1

fix #22624, crash in certain circular type definitions
This was caused by expanding a type's field types in the wrong
environment.

test/core.jl
@@ -177,6 +177,16 @@ struct C21923{T,N}; v::C21923{T,M} where M; end
 struct D21923{T,N}; v::D21923{T}; end
 @test fieldtype(D21923, 1) == D21923
 
+# issue #22624, more circular definitions
+struct T22624{A,B,C}; v::Vector{T22624{Int,A}}; end
+let elT = T22624.body.body.body.types[1].parameters[1]
+    @test elT == T22624{Int64, T22624.var, C} where C
+    elT2 = elT.body.types[1].parameters[1]
+    @test elT2 == T22624{Int64, Int64, C} where C
+    @test elT2.body.types[1].parameters[1] === elT2
+    @test isleaftype(elT2.body.types[1])
+end
+
 # issue #3890
 mutable struct A3890{T1}
     x::Matrix{Complex{T1}}

visr/julia

Ref #22627
(cherry picked from commit 211e8d156c25a081ebfc1b415358126f54bc83f9)

test/core.jl
@@ -177,6 +177,16 @@ struct C21923{T,N}; v::C21923{T,M} where M; end
 struct D21923{T,N}; v::D21923{T}; end
 @test fieldtype(D21923, 1) == D21923
 
+# issue #22624, more circular definitions
+struct T22624{A,B,C}; v::Vector{T22624{Int64,A}}; end
+let elT = T22624.body.body.body.types[1].parameters[1]
+    @test elT == T22624{Int64, T22624.var, C} where C
+    elT2 = elT.body.types[1].parameters[1]
+    @test elT2 == T22624{Int64, Int64, C} where C
+    @test elT2.body.types[1].parameters[1] === elT2
+    @test isleaftype(elT2.body.types[1])
+end
+
 # issue #3890
 mutable struct A3890{T1}
     x::Matrix{Complex{T1}}